c330326b22a44841a56cc54128e197ff69f5a767
Krystian <w.krystian93@gmail.com>
2019-11-18 20:25:06 +0100

    add functions: -addTask -checkTextInput - checkDone

---
+++ index.js
@@ -0,0 +1,38 @@
+new Vue({
+  el: '#root',
+
+  data: {
+    newTask: "",
+    tasks: [],
+  },
+
+  methods: {
+    addTask() {
+      if (this.checkTextInput(this.newTask)) {
+        this.tasks.push({
+          title: this.newTask,
+          completed: false,
+          id: new Date().valueOf(),
+        });
+        this.newTask = "";
+      }
+    },
+
+    checkTextInput(text) {
+      if (text.length) {
+        return true;
+      } else {
+        alert('Fill input field')
+      }
+
+    },
+
+    checkDone(taskID) {
+      this.tasks.find(task => {
+        if (task.id === taskID) {
+          task.completed = !task.completed;
+        }
+      })
+    }
+  },
+})
